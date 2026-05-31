FIX: add pixel re-weighting to prevent skipping agents.

c3jepa_wm/data/data_module.py
@@ -5,7 +5,7 @@
 # %% auto #0
 __all__ = ['DataModule', 'VQDataModule']
 
-# %% ../../nbs/01c_data.data_module.ipynb #943c9dad
+# %% ../../nbs/01c_data.data_module.ipynb #501c06a4
 import os
 
 import numpy as np
@@ -19,7 +19,7 @@ import torch.nn.functional as F
 from .datasets import MultiAgentPOVDataset
 from .transforms import get_transforms
 
-# %% ../../nbs/01c_data.data_module.ipynb #d44f6469
+# %% ../../nbs/01c_data.data_module.ipynb #e32c1e0d
 class DataModule:
     def __init__(self,
                  data_dir: str, 
@@ -89,7 +89,7 @@ class DataModule:
         return colate_fn
     
 
-# %% ../../nbs/01c_data.data_module.ipynb #e03a3e2d
+# %% ../../nbs/01c_data.data_module.ipynb #b826efe5
 class VQDataModule(DataModule):
     def __init__(self, cfg):
         self.cfg = cfg

c3jepa_wm/data/transforms.py
@@ -5,12 +5,12 @@
 # %% auto #0
 __all__ = ['get_transforms']
 
-# %% ../../nbs/01b_data.transforms.ipynb #058b4aac
+# %% ../../nbs/01b_data.transforms.ipynb #18a41e11
 import torch
 import torchvision.transforms.v2 as v2
 import torch.nn.functional as F
 
-# %% ../../nbs/01b_data.transforms.ipynb #ed6e17a9
+# %% ../../nbs/01b_data.transforms.ipynb #d5b9ffcd
 def get_transforms(env= "findgoal", model= "VQVAE"):
 
     if model == "VQVAE" and env == "findgoal":

c3jepa_wm/data/utils.py
@@ -7,11 +7,11 @@ __all__ = ['H_full_url', 'valid_2d_url', 'csi_data_url', 'gdown_download_file', 
            'np_get_csi', 'np_get_all_from', 'np_get_neighbors_csi', 'build_csi_index', 'df_get_csi', 'df_get_all_from',
            'df_get_neighbors_csi', 'save_rollout_hdf5', 'merge_npz_to_hdf5', 'show_batch']
 
-# %% ../../nbs/01d_data.utils.ipynb #049bb123
+# %% ../../nbs/01d_data.utils.ipynb #d4ea21c9
 from fastcore import *
 from fastcore.utils import *
 
-# %% ../../nbs/01d_data.utils.ipynb #05ac8e13
+# %% ../../nbs/01d_data.utils.ipynb #e397b491
 import os
 
 import numpy as np
@@ -23,7 +23,7 @@ from torch.utils.data import DataLoader
 import torchvision.transforms.v2 as v2
 import torch.nn.functional as F
 
-# %% ../../nbs/01d_data.utils.ipynb #c10b04d3
+# %% ../../nbs/01d_data.utils.ipynb #ccb831cd
 def gdown_download_file(url: str, output: str= ".") -> None:
     """Downloads a file from the given URL and saves it to the specified output path.
     Args:
@@ -32,13 +32,13 @@ def gdown_download_file(url: str, output: str= ".") -> None:
     """
     gdown.download(url, output)
 
-# %% ../../nbs/01d_data.utils.ipynb #582cf27d
+# %% ../../nbs/01d_data.utils.ipynb #518de6ed
 H_full_url = "https://drive.google.com/file/d/126gvv5GBgzgG21y19fza5vHBtFGxRWMq/view?usp=sharing"
 valid_2d_url = "https://drive.google.com/file/d/1Dzj9joHNG434-ZQy3lIEBlscFvw_jH0w/view?usp=sharing"
 csi_data_url = "https://drive.google.com/file/d/1bAQEiAvFU-oNeb_UuFO8nOD0NKaL08le/view?usp=sharing"
 
 
-# %% ../../nbs/01d_data.utils.ipynb #39bfc473
+# %% ../../nbs/01d_data.utils.ipynb #5255716d
 def get_h_full(H_full_url: str = H_full_url) -> np.ndarray:
     gdown_download_file(H_full_url, "H_full.npy")
     return np.load("H_full.npy")
@@ -52,7 +52,7 @@ def get_csi_data(csi_data_url: str = csi_data_url) -> pd.DataFrame:
     return pd.read_csv("csi_data.csv")
 
 
-# %% ../../nbs/01d_data.utils.ipynb #b2ba0feb
+# %% ../../nbs/01d_data.utils.ipynb #607a8a53
 def np_get_csi(H_full: np.ndarray, grid_to_idx: dict, tx_grid: tuple, rx_grid: tuple) -> np.ndarray:
     """
     Get CFR for a D2D link between two grid positions.
@@ -86,7 +86,7 @@ def np_get_neighbors_csi(H_full: np.ndarray, grid_to_idx: dict, tx_grid: tuple, 
 
 
 
-# %% ../../nbs/01d_data.utils.ipynb #a4f08607
+# %% ../../nbs/01d_data.utils.ipynb #fa9bf446
 # Assuming df has columns: tx_grid, rx_grid, csi
 # and tuples are stored as (gx, gy)
 
@@ -127,7 +127,7 @@ def df_get_neighbors_csi(csi_data: pd.DataFrame, tx_grid: tuple, radius: int = 1
     )
     return csi_data[mask]
 
-# %% ../../nbs/01d_data.utils.ipynb #18944102
+# %% ../../nbs/01d_data.utils.ipynb #9a3541e9
 import os
 import glob
 
@@ -135,7 +135,7 @@ import numpy as np
 import h5py
 
 
-# %% ../../nbs/01d_data.utils.ipynb #a01e7009
+# %% ../../nbs/01d_data.utils.ipynb #3c35b137
 def save_rollout_hdf5(rollout_idx, save_dict, data_dir):
     """Save one rollout as a group inside a shared HDF5 file."""
     os.makedirs(data_dir, exist_ok=True)
@@ -192,7 +192,7 @@ def merge_npz_to_hdf5(data_dir, out_path):
     print(f"Skipped {skipped} corrupted rollouts")
     
 
-# %% ../../nbs/01d_data.utils.ipynb #f3853c17
+# %% ../../nbs/01d_data.utils.ipynb #fe65d027
 import matplotlib.pyplot as plt
 import numpy as np
 import torchvision

c3jepa_wm/models/control_jepa.py
@@ -5,7 +5,7 @@
 # %% auto #0
 __all__ = []
 
-# %% ../../nbs/02c_models.lewm.ipynb #f00225ec
+# %% ../../nbs/02c_models.lewm.ipynb #fb8642e1
 import torch
 from torch import nn
 from torch.nn import functional as F

c3jepa_wm/models/msg_net.py
@@ -5,18 +5,18 @@
 # %% auto #0
 __all__ = ['Tensor', 'BaseVAE', 'VectorQuantizer', 'ResidualLayer', 'VQVAE']
 
-# %% ../../nbs/02a_models.msg_net.ipynb #54994ccc
+# %% ../../nbs/02a_models.msg_net.ipynb #54771b67
 import torch
 from torch import nn
 from torch.nn import functional as F
 
 
-# %% ../../nbs/02a_models.msg_net.ipynb #8888b440
+# %% ../../nbs/02a_models.msg_net.ipynb #f300aee2
 from typing import List, Callable, Union, Any, TypeVar, Tuple
 
 Tensor = TypeVar('torch.tensor')
 
-# %% ../../nbs/02a_models.msg_net.ipynb #3db47713
+# %% ../../nbs/02a_models.msg_net.ipynb #690996fe
 from torch import nn
 from abc import abstractmethod
 
@@ -45,7 +45,7 @@ class BaseVAE(nn.Module):
     def loss_function(self, *inputs: Any, **kwargs) -> Tensor:
         pass
 
-# %% ../../nbs/02a_models.msg_net.ipynb #ad619ef2
+# %% ../../nbs/02a_models.msg_net.ipynb #64e38566
 class VectorQuantizer(nn.Module):
     def __init__(self, num_embeddings, embedding_dim, beta=0.25, decay=0.99, eps=1e-5):
         super().__init__()
@@ -100,7 +100,7 @@ class VectorQuantizer(nn.Module):
         return quantized_latents.permute(0, 3, 1, 2).contiguous(), vq_loss, perplexity
     
 
-# %% ../../nbs/02a_models.msg_net.ipynb #2980a343
+# %% ../../nbs/02a_models.msg_net.ipynb #37e4b795
 # class VectorQuantizer(nn.Module):
 #     """
 #     Reference:
@@ -301,7 +301,12 @@ class VQVAE(BaseVAE):
         input = args[1]
         vq_loss = args[2]
         perplexity = args[3]
-        recons_loss = F.l1_loss(recons, input)
+        # recons_loss = F.l1_loss(recons, input)
+
+        # Weight non-black pixels more heavily
+        pixel_weights = (input > -0.9).float() * 9.0 + 1.0  # 10x weight on non-black
+        recons_loss = (F.l1_loss(recons, input, reduction='none') * pixel_weights).mean()
+        
 
         loss = recons_loss + vq_loss
         return {'loss': loss,

c3jepa_wm/optimizers/schedulers.py
@@ -5,15 +5,15 @@
 # %% auto #0
 __all__ = ['Scheduler', 'WSDSchedule', 'WarmupCosineSchedule', 'CosineWDSchedule', 'LinearDecaySchedule']
 
-# %% ../../nbs/04b_optimizers.scheduler.ipynb #b38795b1
+# %% ../../nbs/04b_optimizers.scheduler.ipynb #4e501f53
 from fastcore import *
 from fastcore.utils import *
 
-# %% ../../nbs/04b_optimizers.scheduler.ipynb #c3d22ef5
+# %% ../../nbs/04b_optimizers.scheduler.ipynb #ec60ffbe
 from functools import partial
 from torch.optim import Optimizer
 
-# %% ../../nbs/04b_optimizers.scheduler.ipynb #3b67eee6
+# %% ../../nbs/04b_optimizers.scheduler.ipynb #67facbd4
 import torch
 import math
 class Scheduler:
@@ -84,7 +84,7 @@ class Scheduler:
         # Return the base LR (not group-specific)
         return self.base_lr
 
-# %% ../../nbs/04b_optimizers.scheduler.ipynb #d26ce6bf
+# %% ../../nbs/04b_optimizers.scheduler.ipynb #ac2261cb
 import math
 
 

c3jepa_wm/optimizers/utils.py
@@ -5,27 +5,27 @@
 # %% auto #0
 __all__ = ['get_cls', 'get_opt', 'EarlyStopper', 'init_opt']
 
-# %% ../../nbs/04a_optimizers.utils.ipynb #680e0fbe
+# %% ../../nbs/04a_optimizers.utils.ipynb #27fe9482
 from fastcore import *
 from fastcore.utils import *
 
-# %% ../../nbs/04a_optimizers.utils.ipynb #4c535182
+# %% ../../nbs/04a_optimizers.utils.ipynb #1b751715
 from functools import partial
 from torch.optim import Optimizer
 
-# %% ../../nbs/04a_optimizers.utils.ipynb #bfdeea96
+# %% ../../nbs/04a_optimizers.utils.ipynb #78318e49
 import importlib
 def get_cls(module_name, class_name):
     module = importlib.import_module(module_name)
     return getattr(module, class_name)
 
-# %% ../../nbs/04a_optimizers.utils.ipynb #3c2cc6a6
+# %% ../../nbs/04a_optimizers.utils.ipynb #558c8ce5
 def get_opt(cfg, model):
     optimizer_cls = get_cls("torch.optim", cfg.optimizer.name)
     optimizer = optimizer_cls(model.parameters(), lr=cfg.optimizer.lr)
     return optimizer
 
-# %% ../../nbs/04a_optimizers.utils.ipynb #42503121
+# %% ../../nbs/04a_optimizers.utils.ipynb #ef6c4dfd
 # Source - https://stackoverflow.com/a
 # Posted by isle_of_gods, modified by community. See post 'Timeline' for change history
 # Retrieved 2025-11-15, License - CC BY-SA 4.0
@@ -48,7 +48,7 @@ class EarlyStopper:
         return False
 
 
-# %% ../../nbs/04a_optimizers.utils.ipynb #566d410b
+# %% ../../nbs/04a_optimizers.utils.ipynb #16752c59
 import torch
 def init_opt(
     cfg,

c3jepa_wm/trainers/msg_trainer.py
@@ -5,11 +5,11 @@
 # %% auto #0
 __all__ = ['VQVAETrainer']
 
-# %% ../../nbs/05b_trainers.msg_trainer.ipynb #7ecdbd6c
+# %% ../../nbs/05b_trainers.msg_trainer.ipynb #7b8ef946
 import torch
 import torch.nn as nn
 
-# %% ../../nbs/05b_trainers.msg_trainer.ipynb #bde3846f
+# %% ../../nbs/05b_trainers.msg_trainer.ipynb #8d369f38
 import os
 import torch
 import torchvision.utils as vutils